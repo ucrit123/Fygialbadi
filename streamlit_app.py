dbd13b9da4ac3ae0a074cc54ff830ad66834d962
ucrit123 <albozozki@gmail.com>
2025-05-22 14:44:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "Ayo kita belajar pelositi dengan bahasa Python gaskennn)."
 )
 st.image("view/IMG-20250423-WA0014.jpg")
+
+
+st.tittle("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
+    
